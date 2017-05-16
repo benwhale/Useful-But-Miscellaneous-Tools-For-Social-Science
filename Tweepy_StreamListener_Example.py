e04141627aa51ca832478397e10e573869fe7b82
Ben Whale <benwhale@hotmail.co.uk>
2017-05-16 16:02:41 +0100

    Update Tweepy_StreamListener_Example.py
    Quick transitional update: transitioned to variables for the... variable... data which was being entered manually. Still some cleaning to do around that. Moved from two search terms for each thing to two lists of terms. Possible extension could be making it easy to generate multiple save locations rather than just two. Will tweak some more when I have time.

--- Tweepy_StreamListener_Example.py
+++ Tweepy_StreamListener_Example.py
@@ -2,11 +2,11 @@
 """
 Listen to Twitter for multiple tags on multiple different subjects at once.
 
-Set:	yourtagone and yourtagtwo for firstthing
-	yourtagthree and yourtagfour for secondthing
+Set:	list tags1 for firstthing
+	list tags2 for secondthing
 	api keys
-
-Last modified: August 2016
+Forked from:	 crahal
+Last modified: 	May 2017 by benwhale
 
 """
 import tweepy
@@ -29,39 +29,49 @@ from tweepy.streaming import StreamListener
 timestamp_previous= time.strftime("%Y%m%d_%H")
 hourscounter=0
 
+path1 = 'firstthing\\twitterdata\\main\\'
+path2 = 'secondthing\\twitterdata\\main\\'
+tags1 = ['onething','anotherthing']
+tags2 = []
+
 class MyListener(StreamListener):
-    def on_status(self, status):
-        global hourscounter
-        global timestamp_previous
-        timestamp= time.strftime("%Y%m%d_%H")
-        location=None
-        if 'yourtagone' in status.text or 'yourtagtwo' in status.text:
-            location='firstthing\\twitterdata\\main\\'
-        elif 'yourtagthree' in status.text or 'yourtagfour' in status.text:
-            location='secondthing\\twitterdata\\main\\'
-        if location is not None:
-            with open(location+'%s.csv' % timestamp, 'a', encoding='utf8',newline='') as f:
-                if timestamp!=timestamp_previous:
-                    hourscounter+=1
-                    print('Stream has been up for:',hourscounter,' hours')
-                writer = csv.writer(f)
-                status.text=status.text.replace('\n', ' ')
-                status.text=status.text.replace('\r', ' ')
-                if status.user.description is not None:
-                    status.user.description=status.user.description.replace('\n', ' ')
-                    status.user.description=status.user.description.replace('\r', ' ')
-                results = [status.id_str, status.created_at,status.user.screen_name,status.user.name,
-				status.user.description,status.user.statuses_count,status.user.location,
-				status.user.followers_count,status.user.friends_count,status.user.created_at,status.text,status.lang]
-                writer.writerow(results) 
-        timestamp_previous=time.strftime("%Y%m%d_%H")
-    def on_error(self, status):
-        print(status)
+	def on_status(self, status):
+		global hourscounter
+		global timestamp_previous
+		timestamp= time.strftime("%Y%m%d_%H")
+		location=None
+
+
+		if any(tag in status.text for tag in tags1):
+			location= path1
+		if any(tag in status.text for tag in tags2):
+			location= path2
+		if location is not None:
+			with open(location+'%s.csv' % timestamp, 'a', encoding='utf8',newline='') as f:
+				if timestamp!=timestamp_previous:
+				hourscounter+=1
+					print('Stream has been up for:',hourscounter,' hours')
+			writer = csv.writer(f)
+			status.text=status.text.replace('\n', ' ')
+			status.text=status.text.replace('\r', ' ')
+			if status.user.description is not None:
+				status.user.description=status.user.description.replace('\n', ' ')
+				status.user.description=status.user.description.replace('\r', ' ')
+			results = [status.id_str, status.created_at,status.user.screen_name,status.user.name,
+					status.user.description,status.user.statuses_count,status.user.location,
+					status.user.followers_count,status.user.friends_count,status.user.created_at,status.text,status.lang]
+			writer.writerow(results) 
+		timestamp_previous=time.strftime("%Y%m%d_%H")
+	def on_error(self, status):
+        	print(status)
+	
+##Start Main Program	
+	
 twitter_stream = Stream(auth, MyListener())
 while True:
-    try: 
-        twitter_stream.filter(track=['yourtagone','yourtagtwo','yourtagthree','yourtagfour'])
-    except:
-        e = sys.exc_info()[0]
-        print('ERROR:',e) 
-        continue
+	try: 
+        	twitter_stream.filter(track=['yourtagone','yourtagtwo','yourtagthree','yourtagfour'])
+    	except:
+        	e = sys.exc_info()[0]
+        	print('ERROR:',e) 
+        	continue
